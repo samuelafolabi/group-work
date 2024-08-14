002adf9c30fababcca08825a53007d99ca961ec1
samuelafolabi <folaborn2013@gmail.com>
2024-08-14 11:49:21 +0100

    updates secrets

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -7,7 +7,7 @@ function Error() {
 
   return (
     <div>
-      <h1>Something went wrong 😢</h1>
+      <h1>Something went wroong 😢</h1>
       <p>{error.data || error.message}</p>
 
       <LinkButton to="-1">&larr; Go back</LinkButton>
